c01981d4ed71d174b9f820a51ea85032f24e8e73
Joshua Wilder Oakley <43791165+jwoakley@users.noreply.github.com>
2024-09-30 11:21:46 -0700

    update images

--- sketch.js
+++ sketch.js
@@ -2,11 +2,8 @@ var mic;
 var mySvg;
 
 function preload(){
-  mySvg = loadImage("img/mindMaps-01.svg")
+  mySvg = loadImage("img/mindMaps-01.png")
 }
-
-
-
 
 
 function setup() {
@@ -23,15 +20,18 @@ function draw() {
   var r = random(0, 255);
   var g = random(0, 255);
   var b = random(0, 255);
+  var x = random(-1000, 1000);
+  var y = random(-500, 1500)
+
 
 
   var vol = mic.getLevel();
-  // background(mouseX, mouseY, 200);
+  // background(200, 200, 200);
    fill(r,g,b);
   ellipse(windowWidth/2, windowHeight/2, vol*1000, vol*1000);
 
   // imageMode(windowWidth/2, windowHeight/2);
-  image(mySvg, vol*10000, vol*10000);
+  image(mySvg, 10, 10);
 
 
 
