e53d8b8c83cd9dd0c7639085036aeccdbc243a5a
Joshua Wilder Oakley <43791165+jwoakley@users.noreply.github.com>
2024-10-04 16:41:19 -0700

    update

--- sketch.js
+++ sketch.js
@@ -8,39 +8,28 @@ function preload(){
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-
   mic = new p5.AudioIn();
   mic.start();
 }
 
-
-
 function draw() {
-
-
-
-  var r = random(0, 255);
-  var g = random(0, 255);
-  var b = random(0, 255);
- 
-
 
   var ww = (windowWidth/2);
   var wh = (windowHeight/2);
  
   var vol = mic.getLevel();
   var x = cos(vol * 1000.1) - g;
-  var y = x * sin(x * .1) + r;
+  // var y = x * sin(x * .1) + r;
 
   frameRate(5);
 
 
-  background(200, 200, 200);
+  // background(200, 200, 200);
   //  fill(r,g,b);
   // ellipse(windowWidth/2, windowHeight/2, vol*1000, vol*1000);
 
   imageMode(CENTER); //IMAGE MODE CENTER MEASURES FROM CENTER POINT OF IMAGE, must use width/2 height/2 with center
-  image(mmOne, ww, wh, x, y);
+  image(mmOne, ww, wh, x, vol*2000);
 
 
 
